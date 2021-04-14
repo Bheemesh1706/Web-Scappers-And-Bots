29d3492498a9fad03cd0317c46b2e3b631e55206
Bheemesh1706 <bheemeshbala.71@gmail.com>
2021-04-14 13:56:18 +0530

    Extracted links and added it into csv file and also enabled cmd line argument links parsing

--- Wiki/wiki.py
+++ Wiki/wiki.py
@@ -1,10 +1,11 @@
 import requests
-
 from bs4 import BeautifulSoup
+import csv
+import sys
 
 
 
-def content_gathering(uri): 
+def content_gathering(uri):   #Function to request the required web page html content
         pg = requests.get(uri)
 
         if pg.status_code == 200:
@@ -14,17 +15,29 @@ def content_gathering(uri):
             print("Can't Retrieve the contente Due to some error")
         
 
-def img_parser(content):
+def img_parser(content):   #Function to prase the img tag link and store it as a csv file 
 
         img = BeautifulSoup(content,'html.parser')
         img_links = img.find_all('img')
+        link = []
+        fields = ["Id","links"]
+        filename = "img_link.csv"
+        Id = 1
+
 
         for x in img_links:
-            print (x)
+            link.append([Id,x.get('src')])  #Extracting Links and adding it to the list
+            Id+=1
+
+        
+        with open(filename, 'w') as csvfile:  #Creating the csv File and exporting the data to it
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(fields)
+            csvwriter.writerows(link)
 
 
 
-img_parser(content_gathering('https://en.wikipedia.org/wiki/Stock'))
+img_parser(content_gathering(sys.argv[1]))
 
 
 
